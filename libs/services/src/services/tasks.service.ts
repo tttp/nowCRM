0a28022de852d07cc40dbea657c889f262d7b94d
Nick <128822732+ToxicBlades@users.noreply.github.com>
2025-11-05 11:46:09 +0200

    feat: docker fixes to smaller image and build time

--- libs/services/src/services/tasks.service.ts
+++ libs/services/src/services/tasks.service.ts
@@ -2,10 +2,10 @@ import API_ROUTES_STRAPI from "../api-routes/api-routes-strapi";
 import type { Form_Task, Task } from "../types/task";
 import BaseService from "./common/base.service";
 
-class TagsService extends BaseService<Task, Form_Task> {
+class TasksService extends BaseService<Task, Form_Task> {
 	public constructor() {
 		super(API_ROUTES_STRAPI.TASKS);
 	}
 }
 
-export const tagsService = new TagsService();
+export const tasksService = new TasksService();
